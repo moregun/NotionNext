8dde5745601f9c41daf638a91c00035f4a609ee0
moregun <43885511+moregun@users.noreply.github.com>
2024-11-03 22:06:41 +0800

    Update config.js
    Delete
      // 顶部右侧导航暗流
      STARTER_NAV_BUTTON_1_TEXT: 'Sign In',
      STARTER_NAV_BUTTON_1_URL: '/sign-in',
    
      STARTER_NAV_BUTTON_2_TEXT: 'Sign Up',
      STARTER_NAV_BUTTON_2_URL: '/sign-up',

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -24,12 +24,6 @@ const CONFIG = {
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
   STARTER_HERO_BANNER_IMAGE: '', // hero区下方的全宽图
 
-  // 顶部右侧导航暗流
-  STARTER_NAV_BUTTON_1_TEXT: 'Sign In',
-  STARTER_NAV_BUTTON_1_URL: '/sign-in',
-
-  STARTER_NAV_BUTTON_2_TEXT: 'Sign Up',
-  STARTER_NAV_BUTTON_2_URL: '/sign-up',
 
   // 特性区块
   STARTER_FEATURE_ENABLE: true, // 特性区块开关
